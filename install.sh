9ce34e78767fc836bd63892a2ad8511d379f9a10
David Michon <david.michon@gmail.com>
2020-07-27 10:49:49 -0300

    install brew packages with brew bundle

--- install.sh
+++ install.sh
@@ -17,7 +17,42 @@ source install/tmux.sh
 if [ "$(uname)" == "Darwin" ]; then
     echo -e "\\n\\nRunning on macOS"
 
-    source install/brew.sh
+    if test ! "$( command -v brew )"; then
+        echo "Installing homebrew"
+        ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
+    fi
+
+    brew bundle
+
+    # After the install, setup fzf
+    echo -e "\\n\\nRunning fzf install script..."
+    echo "=============================="
+    /usr/local/opt/fzf/install --all --no-bash --no-fish
+
+    # after the install, install neovim python libraries
+    echo -e "\\n\\nRunning Neovim Python install"
+    echo "=============================="
+    pip2 install --user neovim
+    pip3 install --user neovim
+
+    # after the install, install pytz python libraries
+    echo -e "\\n\\nRunning pytz Python install"
+    echo "=============================="
+    pip2 install --user pytz
+    pip3 install --user pytz
+
+    # Change the default shell to zsh
+    zsh_path="$( command -v zsh )"
+    if ! grep "$zsh_path" /etc/shells; then
+        echo "adding $zsh_path to /etc/shells"
+        echo "$zsh_path" | sudo tee -a /etc/shells
+    fi
+
+    if [[ "$SHELL" != "$zsh_path" ]]; then
+        chsh -s "$zsh_path"
+        echo "default shell changed to $zsh_path"
+    fi
+
     source install/osx.sh
 fi
 
