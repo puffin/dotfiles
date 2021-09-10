b618c956b06d8d4140e8a54b263f5e534d72e3cd
puffin <david.michon@gmail.com>
2021-09-10 13:38:05 -0300

    add music title in tmux bar

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -50,6 +50,7 @@ tm_battery="#[fg=$base0F,bg=$base00] ♥ #(battery)"
 tm_vpn=" #(~/.dotfiles/bin/vpn_indicator.sh)"
 tm_date="#[default,bg=$base00,fg=$base0C] %R"
 tm_wclock="#[bg=$base00,fg=$base03] #{world_clock_status}"
+tm_tunes="#[bg=$base00,fg=$base0A] #(osascript -l JavaScript $DOTFILES/applescripts/tunes.js)"
 
 set -g status-left "$tm_session_name"
-set -g status-right "$tm_vpn$tm_wclock$tm_battery $tm_date"
+set -g status-right "$tm_vpn$tm_tunes$tm_wclock$tm_battery $tm_date"
