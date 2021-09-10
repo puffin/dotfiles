030b5bc74c90c22d1f2c3602fac3dea647251dab
puffin <david.michon@gmail.com>
2021-09-10 14:24:23 -0300

    add music player state in tmux bar

--- applescripts/tunes.js
+++ applescripts/tunes.js
@@ -1,14 +1,27 @@
-const symbol = "";
+let symbol = "♫";
 let output = "";
+
 if (Application("Music").running()) {
     const track = Application("Music").currentTrack;
     const artist = track.artist();
     const title = track.name();
-    output = `${symbol} ${title} - ${artist}`.substr(0, 50);
+    const status = Application("Music").playerState();
+    if (status == "playing") {
+        symbol = "▶︎";
+    } else {
+        symbol = "■";
+    }
+    output = ` ${symbol} ${title} - ${artist}`.substr(0, 50);
 } else if (Application("Spotify").running()) {
     const track = Application("Spotify").currentTrack;
     const artist = track.artist();
     const title = track.name();
-    output = `${symbol} ${title} - ${artist}`.substr(0, 50);
+    const status = Application("Spotify").playerState();
+    if (status == "playing") {
+        symbol = "▶︎";
+    } else {
+        symbol = "■";
+    }
+    output = ` ${symbol} ${title} - ${artist}`.substr(0, 60);
 }
 output;
