c7ab2c00b8432b03c431f62b5192c48fdf46e928
puffin <david.michon@gmail.com>
2023-01-19 15:16:22 -0300

    fix symlink

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ if [ "$(uname)" == "Darwin" ]; then
 
     if test ! "$( command -v brew )"; then
         echo "Installing homebrew"
-        ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
 
     brew bundle
