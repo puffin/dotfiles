507d7291b20086f9e7fc1b6acf206962a9b7a3c6
puffin <david.michon@gmail.com>
2025-01-07 19:17:37 -0300

    update zscaler on/off script

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -35,26 +35,55 @@ function unidecode() {
     perl -e "binmode(STDOUT, ':utf8'); print \"$@\""
     echo # newline
 }
+
 function zscaler-off () {
-  UIDLIST=$(/bin/ps -axo uid,args | \
-           /usr/bin/grep '/Applications/Zscaler/Zscaler.app/Contents/MacOS/Zscaler' | \
-           /usr/bin/grep -v grep | \
-           /usr/bin/cut -f1 -d\/)
-
-  for ZUID in $UIDLIST; do
-     echo "Unload Zscaler from tray for UID $ZUID"
-     /bin/launchctl asuser $ZUID /bin/launchctl unload /Library/LaunchAgents/com.zscaler.tray.plist
-  done
-
-
-  # Need to run pkill twice.  Once for /Applications/Zscaler/Zscaler.app/Contents/PlugIns/ZscalerTunnel and once for /Applications/Zscaler/Zscaler.app/Contents/PlugIns/ZscalerService and once for /Applications/Zscaler/Zscaler.app/Contents/MacOS/Zscaler
-  sudo pkill Zscaler
-  sudo pkill Zscaler
-
-  echo "ZScaler is now disabled"
+    launchctl list | awk '($3~"^com[.]zscaler[.]")' | while read pid sta label; do
+        u=$(id -u)
+        (set -x; launchctl bootout "gui/$u/$label")
+    done
+    l=$(sudo launchctl list)
+    <<<"$l" awk '($3~"^com[.]zscaler[.]|com.jamfsoftware.task.Every*")' | while read pid sta label; do
+        (set -x; sudo launchctl bootout "system/$label")
+    done
+    ## tunnel service takes longer to stop, so we check and wait up to 15 seconds
+    if <<<"$l" grep "tunnel" >/dev/null; then
+        printf "waiting for zscaler processes to stop"
+        i=1 && while true; do
+            if test -z "$(sudo launchctl list | awk '($3~"^com[.]zscaler[.]")')"; then
+                echo
+                break
+            fi
+            if test $((i++)) -gt 15; then
+                printf "\nwarning: timed out waiting\n"
+                break
+            fi
+            printf "."
+            if test $((i % 4)) -eq 1; then
+                printf "\b\b\b"
+            fi
+            sleep 1
+        done
+    fi
+    echo "ZScaler is now disabled"
 }
 
 function zscaler-on () {
-  sh /Applications/Zscaler/Zscaler.app/Contents/Resources/load_all_trays.sh
-  echo "ZScaler is now enabled"
+    l=$(sudo launchctl list)
+    if test -z "$(<<<"$l" awk '($3=="com.zscaler.UPMServiceController")')"; then
+        (set -x; sudo launchctl bootstrap system /Library/LaunchDaemons/com.zscaler.UPMServiceController.plist)
+    fi
+    if test -z "$(<<<"$l" awk '($3=="com.zscaler.service")')"; then
+        (set -x; sudo launchctl bootstrap system /Library/LaunchDaemons/com.zscaler.service.plist)
+    fi
+    if test -z "$(<<<"$l" awk '($3=="com.zscaler.tunnel")')"; then
+        (set -x; sudo launchctl bootstrap system /Library/LaunchDaemons/com.zscaler.tunnel.plist)
+    fi
+    if test -z "$(<<<"$l" awk '($3~"com.jamfsoftware.task.Every*")')"; then
+        (set -x; sudo launchctl bootstrap system /Library/LaunchDaemons/com.jamfsoftware.task.1.plist)
+    fi
+    if test -z "$(launchctl list | awk '($3=="com.zscaler.tray")')"; then
+        u=$(id -u)
+        (set -x; launchctl bootstrap gui/$u /Library/LaunchAgents/com.zscaler.tray.plist)
+    fi
+    echo "ZScaler is now enabled"
 }
