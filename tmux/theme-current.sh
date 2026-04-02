d328e303824f05e4cb99caa765e6bc5962e51e55
puffin <david.michon@gmail.com>
2026-04-02 16:35:25 -0300

    increase font and vertical space

--- tmux/theme-current.sh
+++ tmux/theme-current.sh
@@ -1,38 +1,38 @@
-## COLORSCHEME: one light
+## COLORSCHEME: one dark
 
 set-option -g status "on"
 
 # default statusbar colors
-set-option -g status-style bg=default,fg=colour239
+set-option -g status-style bg=default,fg=colour250
 
 # default window title colors
-set-window-option -g window-status-style bg=default,fg=colour229
+set-window-option -g window-status-style bg=default,fg=colour244
 
 # default window with an activity alert
-set-window-option -g window-status-activity-style bg=default,fg=colour241
+set-window-option -g window-status-activity-style bg=default,fg=colour250
 
 # active window title colors
-set-window-option -g window-status-current-style bg=default,fg=colour237
+set-window-option -g window-status-current-style bg=default,fg=colour255
 
 # pane border
-set-option -g pane-active-border-style fg=colour241
-set-option -g pane-border-style fg=colour252
+set-option -g pane-active-border-style fg=colour67
+set-option -g pane-border-style fg=colour240
 
 # message infos (visible while writing command)
-set-option -g message-style bg=default,fg=colour241
+set-option -g message-style bg=default,fg=colour250
 
 # writing commands inactive
-set-option -g message-command-style bg=default,fg=colour124
+set-option -g message-command-style bg=default,fg=colour167
 
 # pane number display
-set-option -g display-panes-active-colour colour241
-set-option -g display-panes-colour colour248
+set-option -g display-panes-active-colour colour67
+set-option -g display-panes-colour colour240
 
 # clock
-set-window-option -g clock-mode-colour colour172
+set-window-option -g clock-mode-colour colour176
 
 # bell
-set-window-option -g window-status-bell-style bg=colour124,fg=colour229
+set-window-option -g window-status-bell-style bg=colour167,fg=colour235
 
 set-option -g status-justify "left"
 set-option -g status-left-style none
@@ -41,8 +41,8 @@ set-option -g status-right-style none
 set-option -g status-right-length "80"
 set-window-option -g window-status-separator ""
 
-set-option -g status-left "#[bg=default,fg=colour66] #S "
+set-option -g status-left "#[bg=default,fg=colour67] #S "
 set-option -g status-right ""
 
-set-window-option -g window-status-current-format "#[bg=default,fg=colour132,bold] #I #W "
-set-window-option -g window-status-format "#[bg=default,fg=colour243] #I #W "
+set-window-option -g window-status-current-format "#[bg=default,fg=colour176,bold] #I #W "
+set-window-option -g window-status-format "#[bg=default,fg=colour244] #I #W "
