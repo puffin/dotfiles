54805edc7edb3ac8c301e49b5300618d97f3a8f9
puffin <david.michon@gmail.com>
2026-04-02 12:23:54 -0300

    cleanup

--- install/link.sh
+++ install/link.sh
@@ -32,19 +32,3 @@ for config in $config_files; do
         ln -s "$config" "$target"
     fi
 done
-
-echo -e "\\n\\nCreating vim symlinks"
-echo "=============================="
-VIMFILES=( "$HOME/.vim:$DOTFILES/config/nvim"
-        "$HOME/.vimrc:$DOTFILES/config/nvim/init.vim" )
-
-for file in "${VIMFILES[@]}"; do
-    KEY=${file%%:*}
-    VALUE=${file#*:}
-    if [ -e "${KEY}" ]; then
-        echo "${KEY} already exists... skipping."
-    else
-        echo "Creating symlink for $KEY"
-        ln -s "${VALUE}" "${KEY}"
-    fi
-done
